b719a5368145d1eb0a0dab9ea69dda385af6ddb1
fabiofpires <fabiopires89@outlook.com>
2022-07-27 22:34:32 -0300

    Finalização do exercício para treino da calculadora IMC

--- IMC/projeto.js
+++ IMC/projeto.js
@@ -3,7 +3,7 @@ function Somar(){
     var peso = window.document.getElementById('txtn2');
     var altura = window.document.getElementById('txtn3');
     
-
+    var nome = prompt('Confirme seu nome novamente...')
     var n1 = Number(peso.value);
     var n2 = Number(altura.value); 
 
@@ -11,7 +11,17 @@ function Somar(){
     var peso2 = n1 / total;
 
     if(peso2 < 18.5){
-        res.innerHTML = `${peso2}<br> Peso baixo da média!`
+        res.innerHTML = `Seu cálculo é: <h1>${peso2.toFixed(3.1)}</h1><br><br> Olá ${nome}.<br> Seu peso está baixo da média!`
+    } else if (peso2 >= 18.5 && peso2 < 24.9 ){
+       res.innerHTML = `Seu cálculo é: <h1>${peso2.toFixed(3.1)}</h1><br><br>  Olá ${nome}.<br> Seu peso está normal!`
+    } else if (peso2 >= 25 && peso2 < 29.9){
+        res.innerHTML = `Seu cálculo é: <h1>${peso2.toFixed(3.1)}</h1><br><br>  Olá ${nome}.<br> Você está com sobrepeso! `
+    } else if (peso2 >= 30 && peso2 < 34.9){
+        res.innerHTML = `Seu cálculo é: <h1>${peso2.toFixed(3.1)}</h1><br><br>  Olá ${nome}.<br> Você está com obesidade Grau I.`
+    } else if (peso2 >= 35 && peso2 < 39.9){
+        res.innerHTML = `Seu cálculo é: <h1>${peso2.toFixed(3.1)}</h1><br><br>  Olá ${nome}.<br> Você está com obesidade Grau II. `
+    } else if (peso2 >= 40) {
+        res.innerHTML = `Seu cálculo é: <h1>${peso2.toFixed(3.1)}</h1><br><br>  Olá ${nome}.<br> Você está com obesidade Grau III ou Mórbida.`
     }
 
  
